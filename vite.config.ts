d8f8eaff18e9ee4a4bb5912c5c507032debd2b67
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2025-12-21 12:40:40 -0500

    manual edits -- not working

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
-import path from 'path';
-
-import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 import i18nextLoader from 'vite-plugin-i18next-loader';
+
+import { dirname , resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __dirname = dirname (fileURLToPath (import.meta.url))
 
 export default defineConfig( {
 	build: {
 		rollupOptions: {
 			input: {
 				main: './index.html',
-				app: './create.html',
+				create: './create.html',
+        features: resolve(__dirname, 'public/features.html'),
+        faq: resolve(__dirname, 'public/faq.html')
 			},
 		},
 	},
@@ -19,7 +25,7 @@ export default defineConfig( {
 	],
 	resolve: {
 		alias: {
-			'~': path.resolve( __dirname, 'src' ),
+			'~': resolve( __dirname, 'src' ),
 		},
 	},
 	worker: {
